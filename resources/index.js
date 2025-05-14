f71bc92b9b8e9072b2dfeb78aac5ddf4110872d4
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2025-05-14 17:11:24 +0700

    homepage setup

--- resources/index.js
+++ resources/index.js
@@ -1,10 +1,22 @@
 const http = require('http');
+const { homeHandler } = require('./handlers/home');
 const port = 3000;
 
+const routes = {
+    '/': homeHandler,
+    './index.html': homeHandler
+}
+
 http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    res.write('Hello JS WORLD');
-    res.end();
-}).listen(port); 
+    const route = routes[req.url];
+    if (typeof route === 'function') {
+        homeHandler(res);
+    } else {
+        res.writeHead(404, {
+            'Content-Type': 'text/html'
+        });
+        res.write('404 Page not Found! ')
+        res.end();
+    }
+
+}).listen(port);
