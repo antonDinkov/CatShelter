f6391a3a1b0cfc92025df68c28d8fd2ded2f58ff
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2025-05-18 19:54:56 +0700

    model.js & cats.json transform

--- resources/handlers/home.js
+++ resources/handlers/home.js
@@ -1,6 +1,7 @@
 const { readTemplate, layout } = require("../util");
-const cats = require('../data/cats.json'); //автоматично парсва json
-const path = require('path')
+/* const cats = require('../data/cats.json'); */ //автоматично парсва json
+const path = require('path');
+const { getCats } = require("../model");
 
 function catFragment(cat) {
     return `
@@ -24,6 +25,7 @@ async function homeHandler(req, res) {
         'Content-Type': 'text/html'
     });
 
+    const cats = await getCats();
     const html = template.replace('%%catContent%%', cats.map(catFragment).join('\n'));
 
     res.write(await layout(html, true));
